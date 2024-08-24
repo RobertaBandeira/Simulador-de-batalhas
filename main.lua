ef713b73f8f81e655f80b6b0e45f1b800d028aa8
RobertaBandeira <roohsouza31@gmail.com>
2024-08-23 21:55:37 -0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,24 @@
+os.execute("chcp 65001")
+
+-- header
+print([[
+==================================================================================
+                     O                                     O
+                {o)xxx|===============-  *  -===============|xxx(o}
+                     O                                     O
+                               SIMULADOR DE BATALHA
+
+==================================================================================
+
+                 Você empunha sua espada e se prepara para lutar.
+                              É hora da batalha!
+]])
+
+-- Obter definição do jogador
+
+-- Obter definição do monstro
+
+-- Apresentar o monstro
+
+-- Começar o loop de batalha
+
